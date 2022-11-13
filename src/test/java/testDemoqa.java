17dbd1556e16e8392c1bdfc57971ede6a7e7afd7
qasniffer0 <qasniffer0@gmail.com>
2022-11-13 13:18:44 +0500

    Update+jpeg

--- src/test/java/testDemoqa.java
+++ src/test/java/testDemoqa.java
@@ -12,7 +12,7 @@ public class testDemoqa {
     String userEmail = "grey@gmail.com";
     String userNumber = "85769042345";
     String currentAddress = "Moskow";
-    String Subjects = "English";
+    String subjects = "English";
 
     @BeforeAll
     static void beforeAll() {
@@ -23,7 +23,6 @@ public class testDemoqa {
 
     @Test
     void fillFormTest() {
-
         open("https://demoqa.com/automation-practice-form");
         executeJavaScript("$('footer').remove()");
         executeJavaScript("$('#fixedban').remove()");
@@ -36,20 +35,17 @@ public class testDemoqa {
         $(".react-datepicker__month-select").selectOption("July");
         $(".react-datepicker__year-select").selectOption("2000");
         $(".react-datepicker__day--020:not(.react-datepicker__day--outside-month)").click();
-        $("#subjectsInput").setValue(Subjects).pressEnter();
+        $("#subjectsInput").setValue(subjects).pressEnter();
         $("#hobbiesWrapper").$(new ByText("Reading")).click();
         $("#uploadPicture").uploadFromClasspath("img/1.jpeg");
         $("#currentAddress").setValue(currentAddress);
         $("#state").click();
-        $("#stateCity-wrapper").$(new ByText("Uttar Pradesh")).click();
+        $("#stateCity-wrapper").$(new ByText("NCR")).click();
         $("#city").click();
-        $("#stateCity-wrapper").$(new ByText("Agra")).click();
+        $("#stateCity-wrapper").$(new ByText("Delhi")).click();
         $("#submit").click();
-
-
         $(".modal-title").shouldHave(text("Thanks for submitting the form"));
-        $(".table-responsive").shouldHave(text("Serg Grey"), text(userEmail), text("20 July,2000"), text("Male"), text(Subjects), text(currentAddress));
-
+        $(".table-responsive").shouldHave(text("Serg Grey"), text(userEmail), text("20 July,2000"), text("Male"), text(subjects), text(currentAddress));
     }
 }
 
